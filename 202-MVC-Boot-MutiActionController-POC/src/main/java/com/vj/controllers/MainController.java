f3c99f47ed09f3356a6f1af9d9320eec31e5132c
vkmuc <vkmuccharla@gmail.com>
2020-06-17 00:41:11 +0530

    Class Level URL,@RequestMapping() GET,POST request methods.

--- 202-MVC-Boot-MutiActionController-POC/src/main/java/com/vj/controllers/MainController.java
+++ 202-MVC-Boot-MutiActionController-POC/src/main/java/com/vj/controllers/MainController.java
@@ -5,26 +5,39 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+//class level URL to differentiate between multiple Controller methods URLs
+@RequestMapping("/main")
 public class MainController {
 	/*
 		Note : We are using same View(wish.jsp) with different URL mapped to different Model and View data
 		We can call this as a Multi-Action-Controller
 	 */	
-	
-	@RequestMapping("/")	//default url pattern
+
+	public MainController() {
+		System.out.println("MainController.MainController()");
+	}
+
+
+	//	@RequestMapping("/")	//default url pattern //default mode : GET
+	@RequestMapping(value = {"/","/home"})	//Multiple URLs for same method
 	public ModelAndView showHome() {
+		System.out.println("MainController.showHome()");
 
 		ModelAndView mav=new ModelAndView();
-		mav.addObject("wish", "This is a wish messenger !!! ").setViewName("wish");
+		mav.addObject("wish", "This is Main Controller !!! ").setViewName("wish");
 		return mav;
 	}
-	
-	@RequestMapping("/morning")
-	public ModelAndView wishMorning() {
+
+	//default mode of any method is GET, to change we have to use method=RequestMethod.POST/GET
+	//	@RequestMapping("/morning")		//default mode : GET
+	@RequestMapping(value = "/morning",method = RequestMethod.GET)	//GET optional
+	public ModelAndView wishMorning1() {
 		//create ModelAndView Object to add attributeName, Object
+		System.out.println("MainController.wishMorning1()  : GET");
 		ModelAndView mav=new ModelAndView();
 		//add attrib name and Object repectively and set the View name
 		mav.addObject("wish", "Good morning, have a great day ahead !! ").setViewName("wish");
@@ -32,8 +45,25 @@ public class MainController {
 		return mav;
 	}
 
-	@RequestMapping("/afternoon")
+
+	//two same URLs allowed only if they use GET and POST modes individually
+	//Post methods will be called when the request if setn as POST from UI with Request Body(DATA)
+	@RequestMapping(value = "/morning",method = RequestMethod.POST)	
+	public ModelAndView wishMorning2() {
+		//create ModelAndView Object to add attributeName, Object
+		System.out.println("MainController.wishMorning2() : POST");
+		ModelAndView mav=new ModelAndView();
+		//add attrib name and Object repectively and set the View name
+		mav.addObject("wish", "Good morning, have a great day ahead !! ").setViewName("wish");
+		//return ModelAndView obj
+		return mav;
+	}
+
+
+
+	@RequestMapping("/afternoon")	//default mode : GET
 	public ModelAndView wishAfternoon() {
+		System.out.println("MainController.wishAfternoon()");
 		ModelAndView mav= new ModelAndView();
 		mav.addObject("wish", "Good afternoon fellas !!!").setViewName("wish");
 		return mav;
@@ -41,22 +71,33 @@ public class MainController {
 
 	//	here, Using ModelAndView in the code makes our code tightly coupled/bound to Spring Framework
 	//	hence we can use Collections Framework to make it Loosely coupled with the Spring Framework
-	@RequestMapping("/evening")
+	@RequestMapping("/evening")	//default mode : GET
 	public String wishEvening(Map<String,Object> map) {
+		System.out.println("MainController.wishEvening()");
 		map.put("wish", "Good evening peeps !!!");
 		return "wish";
 	}
- 
-	@RequestMapping("/night")
+
+	@RequestMapping("/night")	//default mode : GET
 	public String wishNight(Map<String,Object> map) {
+		System.out.println("MainController.wishNight()");
 		map.put("wish", "Good night, watch some movies !!!");
 		return "wish";
 	}
 
-	@RequestMapping("/gnight")
+	@RequestMapping("/gnight")	//default mode : GET
 	public String wishGnight(Model model) {
+		System.out.println("MainController.wishGnight()");
 		model.addAttribute("wish", "Good night, sleep off !!!");
 		return "wish";
+	}
+
+	//same URL available in OsController, we differentiate them with Class level URL pattern
+	@RequestMapping("/ios")	//default mode : GET
+	public String getOwnerOfIos(Model model) {
+		System.out.println("MainController.getOwnerOfIos() : MainController");
+		model.addAttribute("os", "IOS : Apple");
+		return "owner";
 	}
 
 }
--- 202-MVC-Boot-MutiActionController-POC/src/main/java/com/vj/controllers/OsController.java
+++ 202-MVC-Boot-MutiActionController-POC/src/main/java/com/vj/controllers/OsController.java
@@ -5,25 +5,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = {"/os","/system"})
 public class OsController {
 
-	@RequestMapping("/ios")
+	public OsController() {
+		System.out.println("OsController.OsController()");
+	}
+
+	@RequestMapping("/")
+	public String showHome(Model model) {
+		System.out.println("OsController.showHome()");
+		model.addAttribute("os", " This is OS Controller !!!");
+		return "owner";
+	}
+
+	@RequestMapping("/ios")		//default mode : GET
 	public String getOwnerOfIos(Model model) {
+		System.out.println("OsController.getOwnerOfIos()");
 		model.addAttribute("os", "IOS : Apple");
 		return "owner";
 	}
-	
-	@RequestMapping("/android")
+
+	@RequestMapping("/android")	//default mode : GET
 	public String getOwnerOfAndoid(Model model) {
 		model.addAttribute("os", "Android : Google");
 		return "owner";
 	}
-	
-	@RequestMapping("/oxygen")
+
+	@RequestMapping("/oxygen")	//default mode : GET
 	public String getOwnerOfOxygen(Model model) {
 		model.addAttribute("os", "Oxygen : Oneplus");
 		return "owner";
 	}
-	
-	
+
+
 }
