08257495459d2610d7cb30126fa6050137b55d4c
vkmuc <vkmuccharla@gmail.com>
2020-07-14 23:30:41 +0530

    JsonToJava- Jackson API

--- 306-SB-Rest-JavaToJson-JacksonAPI/src/main/java/com/vj/converters/JsonToJava.java
+++ 306-SB-Rest-JavaToJson-JacksonAPI/src/main/java/com/vj/converters/JsonToJava.java
@@ -8,13 +8,28 @@ import com.vj.pojo.Student;
 public class JsonToJava {
 
 	public static void main(String[] args) throws Exception {
+
+		System.out.println(" Json data to One object");
 		convertJsonToObj();
+
+		System.out.println("\n\n Json data to Array of objects");
+		convertJsonDataToObj();
 	}
 
 	private static void convertJsonToObj() throws Exception {
-		File f = new File("src/main/resources/students.json");
+		File f = new File("student.json");
 		ObjectMapper mapper = new ObjectMapper();
 		Student student = mapper.readValue(f, Student.class);
 		System.out.println(student);
+	}
+
+	private static void convertJsonDataToObj() throws Exception {
+		File f = new File("students.json");
+		ObjectMapper mapper = new ObjectMapper();
+		Student[] students = mapper.readValue(f, Student[].class);
+		for (Student student : students) {
+			System.out.println(student);
+		}
+
 	}
 }
