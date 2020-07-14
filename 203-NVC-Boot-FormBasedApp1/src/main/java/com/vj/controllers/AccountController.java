0743f2fd53d83d03c39b316d6c579a482f147aed
vkmuc <vkmuccharla@gmail.com>
2020-07-15 00:43:54 +0530

    Form Binding Object mechanism(Command Class)

--- 203-NVC-Boot-FormBasedApp1/src/main/java/com/vj/controllers/AccountController.java
+++ 203-NVC-Boot-FormBasedApp1/src/main/java/com/vj/controllers/AccountController.java
@@ -18,32 +18,31 @@ public class AccountController {
 	}
 
 	@PostMapping(value = "/register")
-	public String signUpResult(HttpServletRequest req,Model model) {
+	public String signUpResult(HttpServletRequest req, Model model) {
 		String uname = req.getParameter("uname");
-		System.out.println(" Username : "+uname);
+		System.out.println(" Username : " + uname);
 		String pwd = req.getParameter("pwd");
-		System.out.println(" Password : "+pwd);
+		System.out.println(" Password : " + pwd);
 		String email = req.getParameter("email");
-		System.out.println(" Emial : "+email);
+		System.out.println(" Emial : " + email);
 		String phone = req.getParameter("phone");
-		System.out.println(" Phone : "+phone);
+		System.out.println(" Phone : " + phone);
 
 		model.addAttribute("result", " Registration Successful !! ");
 		return "signupresult";
 	}
 
-
 	/**
 	 * HttpServletRequest ---> To capture data coming in Request
-
+	
 	String name = req.getParameter("uname");
-
--> If we have more fields in form then we should write boiler plate code to capture form data.
-
--> In application we will have several forms and every form will have several fields then we should 
+	
+	-> If we have more fields in form then we should write boiler plate code to capture form data.
+	
+	-> In application we will have several forms and every form will have several fields then we should 
 	write lot of boiler plate code in our application.
-
--> To avoid this boiler plate code, Spring MVC provided Form Binding Object mechanism(Command Class).
+	
+	-> To avoid this boiler plate code, Spring MVC provided Form Binding Object mechanism(Command Class).
 	 * 
 	 */
 	@GetMapping(value = "/signup")
@@ -51,11 +50,16 @@ public class AccountController {
 		return "signup";
 	}
 
-	//here User acts as a form binding obj(command cls obj),
-	//	it has the fields with same name as form fields.
-	//DS maps the form fields with Form binding obj properties
+	/**
+	 * 	> here User acts as a form binding obj(command cls obj),
+		> it has the fields with same name as form fields.
+		> DS maps the form fields with Form binding obj properties
+	 * @param user
+	 * @param model
+	 * @return
+	 */
 	@PostMapping(value = "signup")
-	public String signUpResult(User user,Model model) {
+	public String signUpResult(User user, Model model) {
 
 		System.out.println(user);
 
@@ -64,6 +68,5 @@ public class AccountController {
 		return "signupresult";
 
 	}
-
 
 }
